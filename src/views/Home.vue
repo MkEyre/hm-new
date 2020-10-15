65f4aecb027c5b6321504981e2b1e2449f9903a7
没差 <1350935255@qq.com>
2020-10-15 11:39:44 +0800

    首页头部完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,5 +14,36 @@
 export default {};
 </script>
 
-<style>
+<style lang="less" scoped>
+.homeHeader {
+  height: 40px;
+  background-color: red;
+  display: flex;
+  color: #fff;
+  align-items: center;
+  text-align: center;
+
+  .left {
+    width: 60px;
+    .iconnew {
+      font-size: 40px;
+    }
+  }
+  .right {
+    width: 60px;
+    .iconwode {
+      font-size: 25px;
+    }
+  }
+  .center {
+    flex: 1;
+    background-color: rgba(255, 255, 255, 0.5);
+    height: 30px;
+    line-height: 30px;
+    border-radius: 15px;
+    span {
+      margin-left: 10px;
+    }
+  }
+}
 </style>
